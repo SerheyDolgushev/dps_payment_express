2b73f087a5d1465f9b51f915bff8bc16dab29bf4
Serhey Dolgushev <sd@nxc.no>
2013-03-25 20:50:09 +0200

    default DPS settings

--- settings/dpspaymentexpress.ini.append.php
+++ settings/dpspaymentexpress.ini.append.php
@@ -3,12 +3,12 @@
 [LocalShopSettings]
 # Unique username to identify customer account.
 # Assigned on account setup by Payment Express support team.
-PxPayUserID=SD_dev
+PxPayUserID=USER_ID
 
 # Unique key to identify customer and used to encrypt the transaction request
 # with 3DES to protect the transaction information.
 # Assigned on account setup by Payment Express support team.
-PxPayKey=5d6c7039cc2fe4eaead7ba93a34929cb92775fad0560eb08e701849ea4f91120
+PxPayKey=PX_PAY_KEY
 
 # Used to specify the currency to be used: AUD, USD, NZD etc.
 # http://www.paymentexpress.com/Technical_Resources/Ecommerce_Hosted/PxPay.aspx#currencyinput
